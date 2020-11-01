20b28a144484ee4cbb901bed234ae4c9f9c6f9df
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-11-01 14:17:18 +0000

    Initial commit with MathQuill and MathJax support

---
+++ src/app/math-display/math-display.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MathDisplayComponent } from './math-display.component';
+
+describe('MathDisplayComponent', () => {
+  let component: MathDisplayComponent;
+  let fixture: ComponentFixture<MathDisplayComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MathDisplayComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MathDisplayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/math-display/math-display.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+declare var MathQuill: any;
+declare var MathJax: any;
+
+@Component({
+  selector: 'app-math-display',
+  templateUrl: './math-display.component.html',
+  styleUrls: ['./math-display.component.scss']
+})
+export class MathDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    var mathFieldSpan = document.getElementById('math-field');
+    var latexSpan = document.getElementById('latex');
+    var mmlSpan = document.getElementById('mml');
+
+    var MQ = MathQuill.getInterface(2);
+    var mathField = MQ.MathField(mathFieldSpan, {
+      spaceBehavesLikeTab: true,
+      handlers: {
+        edit: function () {
+          latexSpan.textContent = mathField.latex();
+          mmlSpan.textContent = MathJax.tex2mml(mathField.latex());
+        }
+      }
+    });
+  }
+}
