1685ac2fdfdd78f6bc2f474183c5e92a0cac293f
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-11-01 14:27:03 +0000

    raw output

--- src/app/math-display/math-display.component.ts
+++ src/app/math-display/math-display.component.ts
@@ -15,12 +15,14 @@ export class MathDisplayComponent implements OnInit {
     var mathFieldSpan = document.getElementById('math-field');
     var latexSpan = document.getElementById('latex');
     var mmlSpan = document.getElementById('mml');
+    var rawSpan = document.getElementById('raw');
 
     var MQ = MathQuill.getInterface(2);
     var mathField = MQ.MathField(mathFieldSpan, {
       spaceBehavesLikeTab: true,
       handlers: {
         edit: function () {
+          rawSpan.textContent = mathField.text();
           latexSpan.textContent = mathField.latex();
           mmlSpan.textContent = MathJax.tex2mml(mathField.latex());
         }
